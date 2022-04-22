b1e45d79b0005573a1f8a4e7bab143b3acfc2913
polo123456789 <pabsan02@gmail.com>
2022-04-22 12:33:30 -0600

    Mirgrado para usar petite-vue

--- index.js
+++ index.js
@@ -1,63 +1,52 @@
+import {
+    createApp
+} from 'https://unpkg.com/petite-vue@0.2.2/dist/petite-vue.es.js';
+
 // Metadata lines in .csv file
 const irrelevant_lines = 6;
 
 const name_field = 1;
 const attendants_field = 5;
 
-/**
- * @argument text {string}
- */
-const calcularAsistencia = (text) => {
-    const res = document.querySelector("#total");
-    const table = document.querySelector("#result-table");
-    const lines = text.split("\n");
-    let total = 0;
-    let resultTableContent = "";
+createApp({
+    attendants: [],
+    total: 0,
+    calculateAttendance(text) {
+        const lines = text.split("\n");
+        this.total = 0; // Reset if the value was already set
 
-    // Remove blank line at the end
-    lines.pop();
+        lines.pop(); // Remove blank line at the end
 
-    // Ignore metadata
-    for (let i = 0; i < irrelevant_lines; i++) {
-        lines.shift();
-    }
-
-    // Create table header
-    const header = document.querySelector("#table-header");
-    resultTableContent = header.innerHTML;
-    resultTableContent += "<tbody>";
-
-    const rowTemplate = document.querySelector("#table-row");
-    const content = rowTemplate.content.querySelectorAll("td");
-
-    // Create entries
-    lines.forEach((line) => {
-        const fields = line.split(","); 
-        const attendants = fields[attendants_field];
-        const name = fields[name_field];
-        const words = attendants.split(" ");
-
-        if (words.length == 2) {
-
-            const amount = parseInt(words[0]);
-            total += amount;
-            content[0].innerHTML = name;
-            content[1].innerHTML = attendants;
-            resultTableContent += rowTemplate.innerHTML;
+        // Ignore metadata
+        for (let i = 0; i < irrelevant_lines; i++) {
+            lines.shift();
         }
-    });
-    resultTableContent += "</tbody>";
-    table.innerHTML = resultTableContent;
-    res.innerHTML = `En total fueron ${total} asistentes`;
-}
 
-const input = document.querySelector("input#file");
-input.addEventListener("change", (evt) => {
-    const file = evt.target.files[0];
-    const reader = new FileReader();
+        // Create entries
+        lines.forEach((line) => {
+            const fields = line.split(",");
+            const attendants = fields[attendants_field];
+            const name = fields[name_field];
+            const words = attendants.split(" ");
 
-    reader.addEventListener("load", (e) => {
-        calcularAsistencia(e.target.result);
-    });
-    reader.readAsText(file);
-});
+            if (words.length == 2) {
+                const amount = parseInt(words[0]);
+                this.total += amount;
+                this.attendants.push({
+                    name: name,
+                    amount: attendants
+                });
+            }
+        });
+    },
+    generateAttendance(evt) {
+        const file = evt.target.files[0];
+        const reader = new FileReader();
+
+        reader.addEventListener("load", (e) => {
+            this.calculateAttendance(e.target.result);
+        });
+        reader.readAsText(file);
+    },
+
+}).mount("#app");
